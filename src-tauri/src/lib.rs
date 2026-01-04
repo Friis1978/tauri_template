86631fd511751a9163195ed8cd927484c8d1cc5b
Rasmus Friis <36473387+Friis1978@users.noreply.github.com>
2026-01-04 16:59:44 +0100

    feat: add Tailwind CSS configuration and styles
    - Created postcss.config.js for Tailwind CSS and autoprefixer integration.
    - Added tailwind.config.js for Tailwind CSS configuration.
    - Introduced style.css with Tailwind CSS styles for buttons.
    - Updated main.ts to import global styles.
    - Enhanced App.vue with buttons for user management and file operations.
    - Implemented user management commands in lib.rs, including login and file handling.
    - Integrated tauri-plugin-dialog for file operations in the Tauri application.
    - Updated Cargo.toml and Cargo.lock to include new dependencies for dialog and file handling.
    - Modified tauri.conf.json to enhance window properties.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,19 @@
-use tauri::Manager;
+use std::sync::Mutex;
+
+use tauri::{Emitter, Manager};
+use tauri_plugin_dialog::DialogExt;
+use tauri::AppHandle;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_user])
+        .plugin(tauri_plugin_dialog::init())
+        .manage(Mutex::new(User {
+            id: 0,
+            username: "".to_string(),
+            password: "".to_string(),
+        }))
+        .invoke_handler(tauri::generate_handler![get_all_users, get_user, login, open_file, save_file])
         .setup(|app| {
             #[cfg(debug_assertions)] // only include this code on debug builds
     {
@@ -19,17 +28,64 @@ pub fn run() {
 }
 
 #[tauri::command]
-fn get_user() -> User{
-    User {
+fn get_all_users() -> Vec<User>{
+
+    let user1 = User {
         id: 1,
-        name: "Alice".to_string(),
+        username: "Alice".to_string(),
         password: "password123".to_string(),
-    }
+    };
+    let user2 = User {
+        id: 2,
+        username: "Bob".to_string(),
+        password: "securepass".to_string(),
+    };
+    vec![
+        user1,
+        user2,
+    ]
 }
 
-#[derive(serde::Serialize)]
+#[tauri::command]
+fn get_user(state: tauri::State<Mutex<User>>) -> User {
+    let user = &*state.lock().unwrap();
+    user.clone()
+}
+
+#[tauri::command]
+fn login(state: tauri::State<Mutex<User>>, user: User) -> bool {
+    *state.lock().unwrap() = user;
+    println!("{}", state.lock().unwrap().username);
+    true 
+}
+
+#[tauri::command]
+fn save_file(app: AppHandle, content: String){
+    std::thread::spawn(move || {
+        let file_path = app.dialog().file().blocking_save_file().unwrap();
+        let file: String = file_path.to_string();
+
+        std::fs::write(file.clone(), content.clone()).unwrap();
+
+        app.emit("save_state", file).unwrap();
+    });
+}
+
+#[tauri::command]
+fn open_file(app: AppHandle){
+    std::thread::spawn(move || {
+        let file_path = app.dialog().file().blocking_pick_file().unwrap();
+        let file: String = file_path.to_string();
+
+        let content = std::fs::read_to_string(file).unwrap();
+
+        app.emit("content", content).unwrap();
+    });
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 struct User {
     id: u32,
-    name: String,
+    username: String,
     password: String,
 }
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,63 @@
 <script setup lang="ts">
 import { invoke } from "@tauri-apps/api/core";
+import { listen } from "@tauri-apps/api/event";
+import { onMounted } from "vue";
+
+const getAllUsers = async () => {
+  const users = await invoke("get_all_users");
+  console.log(users);
+};
 
 const getUser = async () => {
   const user = await invoke("get_user");
   console.log(user);
 };
+
+const doLogin = async () => {
+  const login = { username: "ras", password: "test", id: 1 };
+  const result = await invoke("login", { user: login });
+  console.log(result);
+};
+
+const openFile = async () => {
+  await invoke("open_file");
+};
+
+const saveFile = async () => {
+  const data =
+    (document.querySelector("#contents") as HTMLTextAreaElement)?.value || "";
+  await invoke("save_file", { content: data });
+};
+
+onMounted(() => {
+  listen("save_state", (event) => {
+    console.log("Save State Event Received:", event.payload);
+  });
+
+  listen("content", (event) => {
+    const textarea = document.querySelector("#contents") as HTMLTextAreaElement;
+    textarea.value = event.payload as string;
+  });
+});
 </script>
 
 <template>
-  <button @click="getUser()">Get User</button>
-  
+  <div class="h-screen w-screen flex flex-col items-center justify-center">
+    <div class="flex p-4 gap-4">
+      <button @click="getAllUsers()">Get All Users</button>
+      <button @click="doLogin()">Login</button>
+      <button @click="getUser()">Get User</button>
+    </div>
+    <div class="flex p-4 gap-4">
+      <button @click="openFile()">Open</button>
+      <button @click="saveFile()">Save</button>
+    </div>
+    <textarea
+      id="contents"
+      class="border-2 border-gray-300 rounded-md p-2 w-96 h-48"
+      placeholder="File content will appear here..."
+    ></textarea>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
